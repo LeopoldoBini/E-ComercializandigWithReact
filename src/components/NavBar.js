7aff8de06ec533cebd8455864509277880e7f97e
LeopoldoBini <raul.leopoldobini@gmail.com>
2021-11-27 18:36:58 -0300

    clase_3

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+
+const NavBar = () => {
+
+    return   <>   
+        <nav style={{display : "flex"}}>
+        <h5 style={{margin : "auto 10px"}}>E-comercializandig</h5>
+        <ul>
+            <li> <a href="#">Ir Aca</a> </li>
+            <li> <a href="#">Alla</a> </li>
+            <li> <a href="#">o Más Alla</a> </li>
+        </ul>
+        </nav>
+
+</>
+}
+export default NavBar;
